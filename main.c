bf3e8c9470edd19ea38f533c113ff389dc63af8b
Matt Finnell <mattfinnell104@gmail.com>
2017-10-02 14:03:05 -0700

    Re-aligned indentation, output to spaces

--- main.c
+++ main.c
@@ -2,9 +2,17 @@
 #include "custom_floats.h"
 
 int main(){
-	int length = 8;
-	int mantis = 3;
-	for(int i = 0; i < 30; i++)
-		printf(" custom_float[%d, %d] [0x%0x]\t%f\n",length,mantis,i,customFloat(i,length,mantis));
-	return 0;
+    int length = 8;
+    int mantis = 3;
+
+    for(int i = 0; i < 30; i++)
+        printf(
+            "custom_float[%d, %d] [0x%0x]\t%f\n",
+            length,
+            mantis,
+            i,
+            customFloat(i, length, mantis)
+        );
+
+    return 0;
 }
